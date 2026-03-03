a8aa49299df77c39420a6b643e3c9bc864642353
Tots8388 <joshua24tuitoek@gmail.com>
2026-03-03 14:28:11 +0300

    Add MySQLServer script

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12345"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print("Error while connecting to MySQL:", e)
+
+finally:
+    try:
+        cursor.close()
+        db.close()
+    except:
+        pass
