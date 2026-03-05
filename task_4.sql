76b5cda0b86ba8906a1ba98df22b546683aadbf3
Tots8388 <joshua24tuitoek@gmail.com>
2026-03-05 12:40:35 +0300

    Complete all SQL tasks and Python script

---
+++ task_4.sql
@@ -0,0 +1,9 @@
+SELECT COLUMN_NAME AS 'Column',
+       COLUMN_TYPE AS 'Type',
+       IS_NULLABLE AS 'Nullable',
+       COLUMN_KEY AS 'Key',
+       COLUMN_DEFAULT AS 'Default',
+       EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = DATABASE();
